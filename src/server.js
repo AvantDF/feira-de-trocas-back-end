654fdc3c8dbfe245eaee1dfc64cb354b8f3dd395
TiagoGomides <gomidestiago@outlook.com>
2025-07-02 18:42:19 -0300

    feat (auth) implementa fluxo de autenticação de usuários Ref: #10

--- src/server.js
+++ src/server.js
@@ -3,14 +3,17 @@ import cors from 'cors'
 import 'dotenv/config'
 import userRoutes from './routes/usersRoutes.js'
 import itemsRoutes from './routes/itemsRoutes.js'
+const { isAuthenticated } = require("../middlewares/isAuthenticated"); 
 
 const app = express()
-
+const PORT = process.env.PORT || 3333;
 app.use(cors())
 app.use(express.json())
 
 // Routes
 app.use('/', userRoutes)
 app.use('/itens', itemsRoutes)
-
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
 export default app
